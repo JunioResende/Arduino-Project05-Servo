4e2c21d22a328994106974428e4efb387a6abc08
JunioResende <junioresende23@gmail.com>
2022-11-20 10:27:35 -0300

    finish

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
+
+int pinServo = 9;
+
+int time = 500;
+
+Servo myServo;
+
+void setup()
+{
+  Serial.begin(9600);
+  myServo.attach(pinServo);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  myServo.write(0);
+  delay(time);
+  myServo.write(90);
+  delay(time);
+  myServo.write(180);
+  delay(time);
 }
